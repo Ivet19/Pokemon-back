15d3669d609a20a07563515a59a83d196f1d17ee
Ivet Lopez <ivet19@live.com>
2025-04-17 01:17:14 +0200

    Cerate checkHealthStatus middleware and its tests

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,10 +1,16 @@
 import express from "express";
 import morgan from "morgan";
+import { checkHealthStatus } from "./middlewares/checkHealthStatus.js";
+import handleEndpointNotFound from "./middlewares/handleEndpointNotFound.js";
 
 const app = express();
 
 app.use(morgan("dev"));
 
 app.use(express.json());
+
+app.get("/", checkHealthStatus);
+
+app.use(handleEndpointNotFound);
 
 export default app;
