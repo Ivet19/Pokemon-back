460d996c486cef97ad0306a2499b067bbb5a5d64
Ivet Lopez <ivet19@live.com>
2025-04-19 13:41:09 +0200

    Add cors and configure allow origin options

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 import { checkHealthStatus } from "./middlewares/checkHealthStatus.js";
 import handleEndpointNotFound from "./middlewares/handleEndpointNotFound.js";
 import pokemonsRouter from "../pokemon/router/pokemonsRouter.js";
 
 const app = express();
+
+app.use(cors({ origin: true, credentials: true }));
 
 app.use(morgan("dev"));
 
